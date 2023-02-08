e8c6c830d1c5e1a3bcf58ff402ba5cb19de9841d
Brandon Dorner <brandon.dorner@anedot.com>
2023-02-07 18:18:36 -0600

    Basic showcase of player api call and display
    This will be built out further but is a temporary showcase.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,26 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import useGetAllPlayers from '../../queries/useGetAllPlayers'
+import { Player } from '../../types/player'
 
 const Home = () => {
-  return <Box>Home Page</Box>
+  return (
+    <Box>
+      <Text>Home Page</Text>
+      {playerData.map((player: Player) => (
+        <Flex key={`player-${player.id}`}>
+          <Heading as="h3">
+            {player.first_name} {player.last_name}
+          </Heading>
+          <Text as="span" paddingLeft={'4px'}>
+            {player.position}
+          </Text>
+          <Text as="span" paddingLeft={'4px'}>
+            {player.team.abbreviation}
+          </Text>
+        </Flex>
+      ))}
+    </Box>
+  )
 }
 
 export default Home
