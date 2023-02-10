d46b8605f2bb230f9f4483bcd7e5c5c5f2fac41f
Brandon Dorner <brandon.dorner@anedot.com>
2023-02-10 11:03:24 -0600

    Create Teams page and query

---
+++ src/pages/Teams/index.tsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import Loading from '../../components/Loading'
+import useGetAllTeams from '../../queries/useGetAllTeams'
+import { Team } from '../../types/team'
+
+const Teams = () => {
+  const { data: teamsData, isLoading } = useGetAllTeams()
+
+  // This can be moved later on in the component that displays the data
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <Box>
+      <Text>Teams Page</Text>
+      {teamsData.map((team: Team) => (
+        <Flex key={`team-${team.id}`}>
+          <Heading as="h3">
+            {team.city} {team.name}
+          </Heading>
+          <Text as="span" paddingLeft={'4px'}>
+            {team.abbreviation}
+          </Text>
+        </Flex>
+      ))}
+    </Box>
+  )
+}
+
+export default Teams
---
+++ src/queries/useGetAllTeams.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+import axiosClient from '../network/axiosClient'
+
+const useGetAllTeams = () => {
+  const response = useQuery({
+    queryKey: ['teams'],
+    queryFn: async () => await axiosClient.get('/teams')
+  })
+
+  return {
+    data: response.data?.data?.data,
+    isLoading: response.isLoading
+  }
+}
+
+export default useGetAllTeams
