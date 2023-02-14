83b7300404dcc5b406242551f205b5ca1f5b6f0e
Brandon Dorner <brandon.dorner@anedot.com>
2023-02-14 11:35:15 -0600

    Update useAllPlayers to use pagination params
    This is setup for the infinite scroll mechanics.

--- src/hooks/useAllPlayers.ts
+++ src/hooks/useAllPlayers.ts
@@ -1,21 +1,48 @@
+import { useEffect, useState } from 'react'
 import useGetAllPlayers from '../queries/useGetAllPlayers'
+import { PaginationValues } from '../types/paginationValues'
 import { Player } from '../types/player'
+import extractData from '../util/extractData'
 import filterNullData from '../util/filterNullData'
 
 type ReturnValue = {
   isLoading: boolean
+  paginationValues: PaginationValues
   players: Player[]
+  totalPlayers: number
 }
 
 const useAllPlayers = (): ReturnValue => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPlayers, setTotalPlayers] = useState(0)
+  const perPage = 25
+
+  const paginationValues = {
+    currentPage,
+    setCurrentPage,
+    perPage
+  }
+
   const { data, isLoading } = useGetAllPlayers()
+
   const filteredPlayers = filterNullData({ data, filterParam: 'firstName' })
 
-  const players = filteredPlayers.map((player) => ({ name: `${player.firstName} ${player.lastName}`, ...player }))
+  useEffect(() => {
+    setTotalPlayers(filteredPlayers.length)
+  }, [filteredPlayers])
+
+  const playersWithName = filteredPlayers.map((player) => ({
+    name: `${player.firstName} ${player.lastName}`,
+    ...player
+  }))
+
+  const displayedPlayers = data ? extractData({ data: playersWithName, currentPage, perPage }) : []
 
   return {
-    players,
-    isLoading
+    paginationValues,
+    isLoading,
+    players: displayedPlayers,
+    totalPlayers
   }
 }
 
---
+++ src/types/paginationValues.ts
@@ -0,0 +1,7 @@
+import { Dispatch, SetStateAction } from 'react'
+
+export type PaginationValues = {
+  currentPage: number
+  perPage: number
+  setCurrentPage: Dispatch<SetStateAction<number>>
+}
