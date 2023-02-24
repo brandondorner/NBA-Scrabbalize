68a840736ec234a2d81af2e3673e289d547005ee
Brandon Dorner <brandon.dorner@anedot.com>
2023-02-24 10:55:30 -0600

    Update folder structure of the player table components

--- src/pages/Players/Tables/FilterablePaginatedPlayersTable.tsx
+++ src/pages/Players/Tables/FilterablePaginatedPlayersTable.tsx
@@ -1,7 +1,7 @@
-import usePlayersColumns from './hooks/usePlayersColumns'
-import Loading from '../../components/Loading'
-import useAllPlayers from '../../hooks/useAllPlayers'
-import FilterablePaginatedTable from '../../components/Table/FilterablePaginatedTable'
+import usePlayersColumns from '../hooks/usePlayersColumns'
+import Loading from '../../../components/Loading'
+import useAllPlayers from '../../../hooks/useAllPlayers'
+import FilterablePaginatedTable from '../../../components/Table/FilterablePaginatedTable'
 
 const FilterablePaginatedPlayersTable = () => {
   const { columns } = usePlayersColumns({ enableSorting: true })
