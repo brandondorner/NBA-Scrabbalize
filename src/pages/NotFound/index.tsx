186b185d83b9eb5a9434b1f878ac0fe8f180b655
Brandon Dorner <brandon.dorner@anedot.com>
2023-02-26 22:05:54 -0600

    `NotFound` style updates
    Make the not found page a little more visually pleasing.

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,17 @@
-import { Box } from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react'
+import { NavLink } from 'react-router-dom'
 
-const Home = () => {
-  return <Box>Page Not Found</Box>
+const NotFound = () => {
+  return (
+    <Flex alignItems={'center'} flexDirection="column" gap={4} height="80vh" justifyContent="center" width={'100%'}>
+      <Heading>Page Not Found</Heading>
+      <Heading as="h4" color={'blue.300'} size="md" textDecoration={'underline'}>
+        <NavLink title="home" to={'/'}>
+          Take me home
+        </NavLink>
+      </Heading>
+    </Flex>
+  )
 }
 
-export default Home
+export default NotFound
