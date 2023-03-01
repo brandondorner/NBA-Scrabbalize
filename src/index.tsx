871eec0cc2b6a695a01e87463db7f9ee53ce0804
Brandon Dorner <brandon.dorner@anedot.com>
2023-03-01 16:43:33 -0600

    Remove unused css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
