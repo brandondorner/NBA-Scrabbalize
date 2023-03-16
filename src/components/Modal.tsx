be7b01ee83abd3546a8bde62ef106b2cfffd1ad4
Brandon Dorner <brandon.dorner@anedot.com>
2023-03-15 23:56:17 -0500

    Create generic Modal component
    This component will be used in displaying the individual player and team stats.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,41 @@
+import {
+  Center,
+  Modal as ChakraModal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spinner
+} from '@chakra-ui/react'
+import { PropsWithChildren } from 'react'
+
+type Props = {
+  header?: string
+  isLoading?: boolean
+  isOpen: boolean
+  onClose: () => void
+} & PropsWithChildren
+
+const Modal = ({ children, header, isLoading, isOpen, onClose }: Props) => {
+  return (
+    <ChakraModal isCentered isOpen={isOpen} onClose={onClose} size="lg">
+      <ModalOverlay />
+      <ModalContent h={'50%'}>
+        {isLoading ? (
+          <Center h="100%">
+            <Spinner />
+          </Center>
+        ) : (
+          <>
+            <ModalHeader textAlign={'center'}>{header}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>{children}</ModalBody>
+          </>
+        )}
+      </ModalContent>
+    </ChakraModal>
+  )
+}
+
+export default Modal
