ed16c901998dfca66e8c658de5fc97ba84398958
Brandon Dorner <brandon.dorner@anedot.com>
2023-03-21 15:23:21 -0500

    Add ViewTeam actions to useTeamColumns
    Now the user can open the teams modal through the teams table.

--- src/pages/Teams/hooks/useTeamsColumns.tsx
+++ src/pages/Teams/hooks/useTeamsColumns.tsx
@@ -1,11 +1,11 @@
 import { useMemo } from 'react'
 import { ColumnDef, createColumnHelper } from '@tanstack/react-table'
-import { Flex, Image, Text, Tooltip } from '@chakra-ui/react'
+import { Button, Flex, Image, Text, Tooltip } from '@chakra-ui/react'
 import { QuestionIcon } from '@chakra-ui/icons'
 import ScoringTooltip from 'components/ScoringToolTip'
 import PlayerAvatar from 'assets/images/player_avatar.png'
 import { Team } from 'types/team'
-
+import useTeamStore from 'state/TeamStore'
 type Props = {
   enableSorting?: boolean
 }
@@ -16,6 +16,7 @@ type ReturnType = {
 
 const useTeamsColumns = ({ enableSorting = false }: Props): ReturnType => {
   const columnHelper = createColumnHelper<Team>()
+  const { setIsTeamModalOpen, setSelectedTeam } = useTeamStore()
 
   const columns = useMemo(
     () => [
@@ -28,9 +29,17 @@ const useTeamsColumns = ({ enableSorting = false }: Props): ReturnType => {
       },
       {
         accessorKey: 'teamLogoUrl',
-        cell: ({ row }: { row: { original: { teamLogoUrl: string } } }) => (
-          // TODO change PlayerAvatar here to something else
-          <Image maxW={'90px'} src={row.original.teamLogoUrl} fallbackSrc={PlayerAvatar} />
+        cell: ({ row }: { row: { original: Team } }) => (
+          <Image
+            cursor={'pointer'}
+            fallbackSrc={PlayerAvatar}
+            maxW={'90px'}
+            onClick={() => {
+              setSelectedTeam(row.original)
+              setIsTeamModalOpen(true)
+            }}
+            src={row.original.teamLogoUrl}
+          />
         ),
         enableSorting: false,
         header: 'Team Logo',
@@ -61,6 +70,26 @@ const useTeamsColumns = ({ enableSorting = false }: Props): ReturnType => {
             </Tooltip>
           </Flex>
         )
+      },
+      {
+        cell: ({ row }: { row: { original: Team } }) => (
+          <Button
+            border="solid 1px white"
+            backgroundColor="initial"
+            onClick={() => {
+              setSelectedTeam(row.original)
+              setIsTeamModalOpen(true)
+            }}
+            _hover={{
+              color: 'black',
+              bg: 'gray.200'
+            }}
+          >
+            View Team
+          </Button>
+        ),
+        enableSorting,
+        header: 'Actions'
       }
     ],
     [columnHelper]
